6fc2f372085e1728b101e2dfd7acc6a1b829017d
mesut <gelukkig.zijn.be@gmail.com>
2020-02-06 05:33:02 +0100

    writing handler.js

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,11 +1,19 @@
-function calculateHandler(event) {
+function calculateHandler(a, b, op) {
+
   // read & process user input from event
+  a = parseInt(a);
+  b = parseInt(b);
+  op = op.toString();
 
   // pass user input through core logic
+  var calculate;
+  calculate = doMath(op, a, b);
 
   // render output to DOM for user
+  document.querySelector('#result').innerHTML = calculate;
 
   // log user action for developers
+  console.log(calculate);
 
   // return true for the browser
   return true;
