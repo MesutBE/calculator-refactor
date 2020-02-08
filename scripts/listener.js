eab85306f1a66e0cc2eddb33daeeece227c07ceb
mesut <gelukkig.zijn.be@gmail.com>
2020-02-08 11:15:47 +0100

    fix-calc-funtion

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,11 +1,1 @@
-document.getElementById('btnCalc').addEventListener('click', calc);
-
-function calc() {
-
-    var a = parseInt(document.querySelector('#value1').value);
-    var b = parseInt(document.querySelector('#value2').value);
-    var op = document.querySelector('#operator').value;
-  
-    calculateHandler(a, b, op);
-    
-  }
+document.getElementById('btnCalc').addEventListener('click', calculateHandler);
